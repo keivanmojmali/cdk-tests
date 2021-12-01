09dd1a0b4149da3b27e6939115e2a3c43004a511
keivanmojmali <mojmalik@gmail.com>
2021-12-01 15:22:44 -0800

    testing

---
+++ hello-cdk/test/hello-cdk.test.js
@@ -0,0 +1,18 @@
+// const { Template } = require('@aws-cdk/assertions');
+// const cdk = require('@aws-cdk/core');
+// const HelloCdk = require('../lib/hello-cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hello-cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
+
