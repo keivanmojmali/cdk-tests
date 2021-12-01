282348bde42e7e5a6315ee09979c777296fac945
keivanmojmali <mojmalik@gmail.com>
2021-12-01 13:45:11 -0800

    sample app

--- simple-app/lib/simple-app-stack.ts
+++ simple-app/lib/simple-app-stack.ts
@@ -1,15 +1,19 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import { Bucket } from '@aws-cdk/aws-s3';
+
+
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const bucket = new Bucket(this,'MySimpleApp', {
+      encryption: BucketEncryption.S3_MANAGED,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+
+
   }
 }
