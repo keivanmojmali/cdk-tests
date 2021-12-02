6958c5b67057a55c55255c50b3bc439df82ad0d6
keivanmojmali <mojmalik@gmail.com>
2021-12-02 14:53:54 -0800

    sample app

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,33 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
-import { Hitcounter } from './hitcounter';
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-      //defines an api resource
     const hello = new lambda.Function(this, 'HelloHandler', {
-      //execution enviroment
       runtime: lambda.Runtime.NODEJS_14_X,
-      //code loaded from 'lambda' directory
       code: lambda.Code.fromAsset('lambda'),
-      //file is "hello", function is "handler"
-      handler:'hello.handler'
-    })
+      handler: 'hello.handler'
+    });
 
-    const helloWithCounter = new Hitcounter(this, 'HelloHitCounter', {
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
       downstream: hello
-    })
+    });
 
-    //defines an API Gateway API resource backed b y our "hello" function.
+    // defines an API Gateway REST API resource backed by our "hello" function.
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
-      
-    })
+    });
   }
 }
-
-
-
