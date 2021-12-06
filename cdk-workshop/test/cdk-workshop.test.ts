c155a500f73c0884a911c4737e2527076d2c762d
keivanmojmali <mojmalik@gmail.com>
2021-12-06 10:46:03 -0800

    sample app

--- cdk-workshop/test/cdk-workshop.test.ts
+++ cdk-workshop/test/cdk-workshop.test.ts
@@ -1,17 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as CdkWorkshop from '../lib/cdk-workshop-stack';
+// import * as cdk from 'aws-cdk-lib';
+// import { Template, Match } from 'aws-cdk-lib/assertions';
+// import * as CdkWorkshop from '../lib/cdk-workshop-stack';
 
-test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
-  // THEN
+// test('SQS Queue and SNS Topic Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
+//   // THEN
 
-  const template = Template.fromStack(stack);
+//   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
-});
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+//   template.resourceCountIs('AWS::SNS::Topic', 1);
+// });
