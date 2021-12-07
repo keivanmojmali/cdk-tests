41c172dc0d2b38b516af2071f751ee8702fc287f
keivanmojmali <mojmalik@gmail.com>
2021-12-07 15:50:30 -0800

    sample jest app

--- jest-udemy/test/unit/todo.controller.test.js
+++ jest-udemy/test/unit/todo.controller.test.js
@@ -1,5 +1,7 @@
 const TodoController = require("../../todo.controller");
 
-describe("TodoController.createTodo") {
-    
-}
+describe("TodoController.createTodo", () =>{
+    it("should have a createTodo function", ()=>{
+        expect(typeof TodoController.createTodo).toBe("function");
+    });
+});
