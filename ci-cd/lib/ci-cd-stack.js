1ea71af48106c19407b6e5bdb31b55393645d237
keivanmojmali <mojmalik@gmail.com>
2021-12-08 12:19:01 -0800

    jest test app

--- ci-cd/lib/ci-cd-stack.js
+++ ci-cd/lib/ci-cd-stack.js
@@ -1,17 +1,23 @@
 const cdk = require('@aws-cdk/core');
 const { CodePipeline, CodePipelineSource, ShellStep } = require('@aws-cdk/pipelines');
+const { MyPipelineAppStage } = require('./my-pipeline-app-stage');
 
  class MyPipelineStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'ci-cd',
+      pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "098329158095", region: "us-west-1" }
+    }));
+
   }
 }
 
