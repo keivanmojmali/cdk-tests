04c4e7bcc61475f8dd7b711cf5a20e8b14b645ec
keivanmojmali <mojmalik@gmail.com>
2021-12-08 14:24:40 -0800

    jest test app

--- jest-udemy/app.js
+++ jest-udemy/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const todoRoutes = require("./routes/todo.routes");
+
+app.use("/todos",todoRoutes);
 
 app.get("/", (req,res)=>{
     res.json("Hello World");
@@ -8,3 +11,5 @@ app.get("/", (req,res)=>{
 app.listen(3000, ()=>{
     console.log("Server is now running");
 })
+
+module.exports = app;
