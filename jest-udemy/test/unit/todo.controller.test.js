117db2429748dfaf3e04342d80b2f3c8e7ff5041
keivanmojmali <mojmalik@gmail.com>
2021-12-08 12:43:14 -0800

    jest test app

--- jest-udemy/test/unit/todo.controller.test.js
+++ jest-udemy/test/unit/todo.controller.test.js
@@ -2,8 +2,16 @@ const TodoController = require("../../todo.controller");
 const TodoModel = require("../../model/todo.model");
 const httpMocks = require("node-mocks-http");
 const newTodo = require("../mock-data/new-todo.json")
+let req, res, next;
 
 TodoModel.create = jest.fn();
+
+beforeEach(()=>{
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+})
+
 
 describe("TodoController.createTodo", () =>{
     it("should have a createTodo function", ()=>{
@@ -11,13 +19,15 @@ describe("TodoController.createTodo", () =>{
     });
 
     it("should call TodoModel.create", ()=>{
-        let req, res, next;
-        req = httpMocks.createRequest();
-        res = httpMocks.createResponse();
-        next = null;
         TodoController.createTodo(res,req,next);
-        expect(TodoModel.create).toBeCalled()
+        expect(TodoModel.create).toBeCalled(newTodo)
+        req.body = newTodo;
         
     })
-
+    it("should return 201 response code",()=>{
+        req.body = newTodo;
+        TodoController.createTodo(req,res,next);
+        expect(res.statusCode).toBe(201)
+        expect(res._isEndCalled()).toBeTruthy();
+    })
 });
